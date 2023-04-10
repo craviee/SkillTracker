1391db6a121da3504e1617ef1cfe76320143ed8a
Daniel Craviée De Abreu Vieira <dcvieira@latam.stefanini.com>
2023-04-10 18:56:02 -0300

    Basic models implemented

---
+++ backend/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace SkillTracker.Models;
+
+public class User
+{
+    public decimal Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public string Email { get; set; }
+    public int Role { get; set; }
+    public bool IsEnabled { get; set; }
+}
---
+++ backend/Models/UserSkill.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillTracker.Models;
+
+public class UserSkill
+{
+    public decimal Id { get; set; }
+    public decimal UserId { get; set; }
+    public decimal SkillId { get; set; }
+    public int Rate { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
