54cf41116ae25e5c1fa31ce20ef002150bab506f
Daniel Craviée De Abreu Vieira <dcvieira@latam.stefanini.com>
2023-04-13 21:12:59 -0300

    DbContex and Entity classes

---
+++ backend/Entities/SkilltrackerContext.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace SkillTracker.Entities;
+
+public partial class SkilltrackerContext : DbContext
+{
+    public SkilltrackerContext()
+    {
+    }
+
+    public SkilltrackerContext(DbContextOptions<SkilltrackerContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Group> Groups { get; set; }
+
+    public virtual DbSet<Skill> Skills { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<Userskill> Userskills { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Group>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("groups");
+
+            entity.HasIndex(e => e.CreatedBy, "groups_users_created_fk");
+
+            entity.HasIndex(e => e.UpdatedBy, "groups_users_updated_fk");
+
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("datetime")
+                .HasColumnName("Created_At");
+            entity.Property(e => e.CreatedBy).HasColumnName("Created_By");
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("Updated_At");
+            entity.Property(e => e.UpdatedBy).HasColumnName("Updated_By");
+
+            entity.HasOne(d => d.CreatedByNavigation).WithMany(p => p.GroupCreatedByNavigations)
+                .HasForeignKey(d => d.CreatedBy)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("groups_users_created_fk");
+
+            entity.HasOne(d => d.UpdatedByNavigation).WithMany(p => p.GroupUpdatedByNavigations)
+                .HasForeignKey(d => d.UpdatedBy)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("groups_users_updated_fk");
+        });
+
+        modelBuilder.Entity<Skill>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("skills");
+
+            entity.HasIndex(e => e.GroupId, "Skills_groups_Id_fk");
+
+            entity.HasIndex(e => e.CreatedBy, "Skills_users_created_fk");
+
+            entity.HasIndex(e => e.UpdatedBy, "Skills_users_updated_fk");
+
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("datetime")
+                .HasColumnName("Created_At");
+            entity.Property(e => e.CreatedBy).HasColumnName("Created_By");
+            entity.Property(e => e.GroupId).HasColumnName("Group_Id");
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("Updated_At");
+            entity.Property(e => e.UpdatedBy).HasColumnName("Updated_By");
+
+            entity.HasOne(d => d.CreatedByNavigation).WithMany(p => p.SkillCreatedByNavigations)
+                .HasForeignKey(d => d.CreatedBy)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Skills_users_created_fk");
+
+            entity.HasOne(d => d.Group).WithMany(p => p.Skills)
+                .HasForeignKey(d => d.GroupId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("Skills_groups_Id_fk");
+
+            entity.HasOne(d => d.UpdatedByNavigation).WithMany(p => p.SkillUpdatedByNavigations)
+                .HasForeignKey(d => d.UpdatedBy)
+                .HasConstraintName("Skills_users_updated_fk");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("users");
+
+            entity.HasIndex(e => e.Email, "Users_email_unique").IsUnique();
+
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("datetime")
+                .HasColumnName("Created_At");
+            entity.Property(e => e.IsEnabled)
+                .IsRequired()
+                .HasDefaultValueSql("'1'")
+                .HasColumnName("Is_Enabled");
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.Role).HasDefaultValueSql("'1'");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("Updated_At");
+        });
+
+        modelBuilder.Entity<Userskill>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("userskills");
+
+            entity.HasIndex(e => e.SkillId, "UserSkills_skills_fk");
+
+            entity.HasIndex(e => e.UserId, "UserSkills_users_fk");
+
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("datetime")
+                .HasColumnName("Created_At");
+            entity.Property(e => e.SkillId).HasColumnName("Skill_Id");
+            entity.Property(e => e.UserId).HasColumnName("User_Id");
+
+            entity.HasOne(d => d.Skill).WithMany(p => p.Userskills)
+                .HasForeignKey(d => d.SkillId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("UserSkills_skills_fk");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Userskills)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("UserSkills_users_fk");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
