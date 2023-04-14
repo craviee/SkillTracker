744e3b36e68b5bf0adf87f7fb97d128f38a53e5f
Daniel Craviée De Abreu Vieira <dcvieira@latam.stefanini.com>
2023-04-13 21:14:20 -0300

    Add DB Service

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore.Extensions;
+using SkillTracker.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddEntityFrameworkMySQL().AddDbContext < SkilltrackerContext > (options => {
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty);
+});
 
 var app = builder.Build();
 
